2379a4d55503fd1655232610e46bf1901d9c9758
vov62 <avivovgen1@gmail.com>
2024-10-21 17:13:04 +0300

    deploy try3

--- deploy/server-script.sh
+++ deploy/server-script.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
+
+# Update the instance 
 yum update -y
 yum install -y python3 python3-pip git
 
 # Clone the GitHub repository
 git clone https://github.com/vov62/PokeApi-Project-Python-AWS-DynamoaDB-Terraform /home/ec2-user/app
 
-# Install Python dependencies
 cd /home/ec2-user/app
-pip3 install -r requirements.txt
-pip3 install boto3
 
-# Export the table name to a file from Terraform outputs
-echo "$(terraform output -raw tableid)" > /home/ec2-user/app/terraform_output.txt
+# Install Python dependencies
+pip3 install -r requirements.txt
+pip3 install boto3 requests
+
+# Ensure that the correct version of Python is used and that the environment is ready
+which python3
+python3 --version
+pip3 --version
 
 # Run the application in the background
-nohup python3 poke_task.py --draw yes &  
+nohup python3 main.py --draw yes &  
 
