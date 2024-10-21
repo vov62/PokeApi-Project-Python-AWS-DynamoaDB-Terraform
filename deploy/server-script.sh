acd194cbe1f35648732bb583f33c30026653cea9
vov62 <avivovgen1@gmail.com>
2024-10-21 21:47:17 +0300

    fix deploy

--- deploy/server-script.sh
+++ deploy/server-script.sh
@@ -2,14 +2,12 @@
 
 # Update the instance 
 yum update -y
-yum install -y python3 python3-pip git unzip
+yum install -y python3 python3-pip git
 
-# Install Terraform
-TERRAFORM_VERSION="1.9.7"  
-wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-mv terraform /usr/local/bin/
-chmod +x /usr/local/bin/terraform
+cd /home/ec2-user
+mkdir -p /home/ec2-user/.aws
+touch /home/ec2-user/.aws/credentials
+touch /home/ec2-user/.aws/config
 
 # Clone the GitHub repositor
 git clone https://github.com/vov62/PokeApi-Project-Python-AWS-DynamoaDB-Terraform /home/ec2-user/app
@@ -19,11 +17,6 @@ cd /home/ec2-user/app
 # Install Python dependencies
 pip3 install -r requirements.txt
 pip3 install boto3 requests
-
-# Set the DynamoDB table name as an environment variable
-export DYNAMODB_TABLE_NAME="$(terraform output -raw tableid)"
-
-
 
 # Ensure that the correct version of Python is used and that the environment is ready
 which python3
