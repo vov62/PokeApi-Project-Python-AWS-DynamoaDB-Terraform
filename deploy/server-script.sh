27022c7bddfe75b241295286b51870971bfb259a
vov62 <avivovgen1@gmail.com>
2024-10-21 17:59:18 +0300

    deploy fix5

--- deploy/server-script.sh
+++ deploy/server-script.sh
@@ -18,6 +18,9 @@ which python3
 python3 --version
 pip3 --version
 
+echo "$(terraform output -raw tableid)" > /home/ec2-user/app/terraform_output.txt
+
+
 # Run the application in the background
 nohup python3 main.py --draw yes &  
 
