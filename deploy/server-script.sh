252c0dbe6acbadbf9b93fc5582691ff1aa29aac2
vov62 <avivovgen1@gmail.com>
2024-10-28 21:43:04 +0200

    edit readme file

--- deploy/server-script.sh
+++ deploy/server-script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Update the instance 
 yum update -y
 yum install -y python3 python3-pip git
 
+# create directory for entering aws credentials
 cd /home/ec2-user
 mkdir -p /home/ec2-user/.aws
 cd .aws/
@@ -11,7 +11,6 @@ touch credentials
 touch config
 cd ..
 
-# Clone the GitHub repositor
 git clone https://github.com/vov62/PokeApi-Project-Python-AWS-DynamoaDB-Terraform /home/ec2-user/app
 
 cd /home/ec2-user/app
@@ -20,10 +19,4 @@ cd /home/ec2-user/app
 pip3 install -r requirements.txt
 pip3 install boto3 requests
 
-# Ensure that the correct version of Python is used and that the environment is ready
-which python3
-python3 --version
-pip3 --version
-
-# Run the application in the background
 nohup python3 main.py --draw yes &  
